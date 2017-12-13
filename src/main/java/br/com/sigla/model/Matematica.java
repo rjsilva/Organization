97842e7a389bba4f6ace145aaab46408f9964d43
Ronaldo <servicoyaman723106@userede.com.br>
2017-12-13 18:00:38 -0200

    master

---
+++ src/main/java/br/com/sigla/model/Matematica.java
@@ -0,0 +1,15 @@
+package br.com.sigla.model;
+
+public class Matematica {
+	
+	public void somar(int number1, int number2) {
+		double result = number1 + number2;
+		System.out.println(result);
+	}
+	
+	public void subtrair(int number1, int number2) {
+		double result = number1 - number2;
+		System.out.println(result);
+	}
+
+}
