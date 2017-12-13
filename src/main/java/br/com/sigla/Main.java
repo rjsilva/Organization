03fe98ce6cea9cad7f46f8ebea594b2ccc84bd1f
Ronaldo <servicoyaman723106@userede.com.br>
2017-12-13 17:29:07 -0200

    first

---
+++ src/main/java/br/com/sigla/Main.java
@@ -0,0 +1,16 @@
+package br.com.sigla;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        somar(2, 3);
+	}
+	
+	public static void somar(int number1, int number2) {
+		double result = number1 + number2;
+		
+		System.out.println(result);
+	}
+
+}
---
+++ src/test/java/run/TesteSomar.java
@@ -0,0 +1,14 @@
+package run;
+
+import br.com.sigla.Main;
+
+public class TesteSomar {
+	
+	private Main main;
+	
+	
+	public void somar() {
+		main.somar(2, 4);
+	}
+
+}
