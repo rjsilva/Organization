b60b6f15c360ace10183210c2c3b69230ce8ec5e
Ronaldo <servicoyaman723106@userede.com.br>
2017-12-13 17:35:37 -0200

    second

--- src/test/java/run/TesteSomar.java
+++ src/test/java/run/TesteSomar.java
@@ -1,12 +1,14 @@
 package run;
 
+import org.junit.Test;
+
 import br.com.sigla.Main;
 
 public class TesteSomar {
 	
 	private Main main;
 	
-	
+	@Test
 	public void somar() {
 		main.somar(2, 4);
 	}
